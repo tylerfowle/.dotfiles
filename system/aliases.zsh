0848a0a25724f5277530866a36218b9d57e50840
tylerfowle <tylerfowle@gmail.com>
2018-03-14 12:52:51 -0500

    remove chpwd and htb --no long use, update dtags aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -21,8 +21,6 @@ alias editdot="e ~/.dotfiles"
 alias editvim="e ~/.dotfiles/vim/vimrc.symlink"
 
 # dtags - tag and cd to directories
-# alias dt=". dtags"
-# alias dtags="~/go/src/github.com/tylerfowle/dtags/dtags"
 alias dt=". ~/go/src/github.com/tylerfowle/dtags/dt"
 
 # taskpaper - quick access to edit tasks in vim
@@ -49,27 +47,6 @@ function colorcodes() {
   for i in {0..255}; do; printf "\x1b[38;5;${i}mcolour${i}\x1b[0m\n"; done
 }
 
-# run whenever cd'ing
-# set the first pane to show ls
-function chpwd() {
-
-  if [[ $TMUX ]]; then
-    # get currently active items
-    session=$(tmux display -p '#S')
-    window=$(tmux display -p '#I')
-    pane=$(tmux display -p '#P')
-    current_path=$PWD
-    active_pane=$(tmux display -pt "${TMUX_PANE:?}" "#{pane_index}")
-
-    # dont run if in pane 1
-    if [[ "$active_pane" -ne 1 ]]; then
-      tmux send-keys -t "$session:$window.1" "cd $current_path;clear;list" Enter
-      tmux select-pane -t "$active_pane"
-    fi
-  fi
-
-}
-
 # find [string], in current directory, recursively, and fzf results
 function strfindr() {
   grep --exclude-dir=.git -r . -e "$1" | fzf
@@ -80,23 +57,17 @@ function filefindr() {
   find . -type d -name "$1" -print | xargs du -shc | gsort -h
 }
 
-# html-beautify all "*.html" files in current directory
-function htb() {
-  for f in $(find . -name '*.html'); do html-beautify -r "$f"; done;
-  }
-
-
-  #keep the last focused finder window up to date when cd in terminal
-  function chpwd() {
-    osascript -e 'on run pwd
-    set f to posix file pwd
-    tell app "Finder"
-    if number of Finder windows is 0 then
-      open f
-    else
-      set target of window 1 to f
-    end
-  end
-end' "$PWD" > /dev/null
-}
+##keep the last focused finder window up to date when cd in terminal
+#function chpwd() {
+#  osascript -e 'on run pwd
+#  set f to posix file pwd
+#  tell app "Finder"
+#    if number of Finder windows is 0 then
+#      open f
+#    else
+#      set target of window 1 to f
+#    end
+#  end
+#  end' "$PWD" > /dev/null
+#}
 
