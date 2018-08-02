c156cd9bb0309364d81a1838856c35ff59cc992d
tylerfowle <tylerfowle@gmail.com>
2018-08-02 09:23:23 -0500

    update readme

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,3 @@
 # GIT Aliases
 alias clean='git com && git branch --merged | grep -v "\*" | xargs -n 1 git branch -d && clr'
-alias readme='date=_$(date +"%m-%d-%Y_%I-%M-%S") && echo $date && git cob update-readme$date && git ac "update readme" && git comp && git merge update-readme$date && git push'
+alias gitreadme='date=_$(date +"%m-%d-%Y_%I-%M-%S") && echo $date && git cob update-readme$date && git ac "update readme" && git comp && git merge update-readme$date && git push'
