ea2b373bfab380b35a2e8c94fe8dc6e3d1bdc293
tylerfowle <tylerfowle@gmail.com>
2018-09-11 14:12:20 -0500

    update time output

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,8 @@
+autoload -Uz colors && colors
+
 export EDITOR='nvim'
 export HTML_TIDY=$HOME/.tidyrc
 export TODO_DIR=$HOME/Dropbox/tasks
 export TODO_FILE=$TODO_DIR/inbox.md
-export TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S'
+export TIMEFMT=$'\n\[%J\]\nreal\t%*Es\nuser\t%U\nsys\t%S'
+# export TIMEFMT="$fg[green]%J$reset_color \ntime: $fg[blue]%*Es$reset_color, cpu: $fg[blue]%P$reset_color"
