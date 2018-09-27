b28c4d7f33f70934ede663ef34d1b89825ae5de4
tylerfowle <tylerfowle@gmail.com>
2018-09-27 07:57:59 -0500

    update python install

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,3 @@
-pip install neovim pep8 flake8 pyflakes pylint isort black jedi matplotlib iterm2 pygame selenium
+#!/usr/bin/env bash
+pip2 install neovim pep8 flake8 pyflakes pylint isort black jedi matplotlib iterm2 pygame selenium
+pip3 install neovim pep8 flake8 pyflakes pylint isort black jedi matplotlib iterm2 pygame selenium
