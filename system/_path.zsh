28c003b1720f47f1a12ce127a18d8912c2c27691
tylerfowle <tylerfowle@gmail.com>
2018-10-03 15:01:23 -0500

    update readme

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/Repos/fed-bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/Repos/fed/fed-bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
