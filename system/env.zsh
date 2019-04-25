707725725790a86c855f046c799d1caeedfb02a5
tylerfowle <tylerfowle@gmail.com>
2019-04-25 16:32:00 -0500

    update golem - remove gulp, update todo task path, add local vimrc sourcing

--- system/env.zsh
+++ system/env.zsh
@@ -2,7 +2,7 @@ autoload -Uz colors && colors
 
 export EDITOR='nvim'
 export HTML_TIDY=$HOME/.tidyrc
-export TODO_DIR=$HOME/Dropbox/tasks
+export TODO_DIR=$HOME/Repos/tasks
 export TODO_FILE=$TODO_DIR/inbox.md
 export TIMEFMT=$'\n\[%J\]\nreal\t%*Es\nuser\t%U\nsys\t%S'
 # export TIMEFMT="$fg[green]%J$reset_color \ntime: $fg[blue]%*Es$reset_color, cpu: $fg[blue]%P$reset_color"
